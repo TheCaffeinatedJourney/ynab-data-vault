6875d3cfce18fb3fca2e2449dcea4e625a8dc2b5
TheCaffeinatedJourney <42365619+TheCaffeinatedJourney@users.noreply.github.com>
2025-03-18 00:18:58 -0400

    Update config.py to pull api key and budget id from .env

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Read values from the environment
+YNAB_API_KEY = os.getenv("YNAB_API_KEY")
+BUDGET_ID = os.getenv("BUDGET_ID")
+
+BASE_URL = "https://api.ynab.com/v1"
